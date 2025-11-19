fee97812b5053c1fe6eba38aa4e6122a4d6153d1
samped <michaelsamuelpedro@gmail.com>
2025-11-19 16:20:45 +0100

    ui fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const jetbrainsMono = JetBrains_Mono({
 })
 
 export const metadata: Metadata = {
-  title: 'Irys Portfolio',
+  title: 'phos',
   description: 'Portfolio showcasing creative work on Irys',
 }
 
